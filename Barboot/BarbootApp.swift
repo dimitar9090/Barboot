ab9619938b46bf754af4bdd1f7e1f3c84ca11c19
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-03-02 22:50:03 +0200

    Initial Commit

---
+++ Barboot/BarbootApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarbootApp.swift
+//  Barboot
+//
+//  Created by Dimitar Angelov on 2.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BarbootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

041b754a3a0e7db797f572bc182003100a8ad2d2
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-08-26 18:39:04 +0300

    i add scoreboard and some new buttons

--- Barboot/DiceGameView.swift
+++ Barboot/DiceGameView.swift
@@ -13,70 +13,127 @@ struct DiceGameView: View {
     @State private var holdDiceTwo = false
     @State private var holdDiceThree = false
     @State private var resetAfterZeroPoints = false
-    @State private var tokens = 10 // Добавяме токени
-    @State private var gameOver = false // Флаг за показване на Game Over
+    @State private var tokens = 10
+    @State private var gameOver = false
+    
+    @State private var playerName = ""
+    @State private var showLeaderboard = false
+    @State private var leaderboard: [PlayerScore] = []
 
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Барбут")
-                .font(.largeTitle)
-            Spacer()
-            Text("Текущи точки: \(currentPoints)")
-                .font(.title)
-            Text("Общо точки: \(totalPoints)")
-                .font(.title)
-            Text("Токени: \(tokens)") // Изписване на токените
-                .font(.title)
-            Spacer()
-            if gameOver {
-                Text("Game Over")
+        ZStack {
+            Color.pink
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                Text("Барбут")
                     .font(.largeTitle)
-                    .foregroundColor(.red)
-                    .padding()
-                Text("Общ брой точки: \(totalPoints)")
+                Spacer()
+                Text("Текущи точки: \(currentPoints)")
                     .font(.title)
-                    .padding()
-                Button("ОК") {
-                    resetGame() // Рестартиране на играта при натискане на бутона
-                }
-                .font(.title)
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-            } else {
-                HStack(spacing: 5.0) {
-                    DiceView(number: diceOne, rolling: $rolling, held: holdDiceOne)
-                    DiceView(number: diceTwo, rolling: $rolling, held: holdDiceTwo)
-                    DiceView(number: diceThree, rolling: $rolling, held: holdDiceThree)
+                Text("Общо точки: \(totalPoints)")
+                    .font(.title)
+                Text("Токени: \(tokens)")
+                    .font(.title)
+                Spacer()
+                if gameOver {
+                    Text("Game Over")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                        .padding()
+                    Text("Общ брой точки: \(totalPoints)")
+                        .font(.title)
+                        .padding()
+                    
+                    TextField("Вашето име", text: $playerName)
+                        .font(.title)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    HStack {
+                        Button("Запази резултата") {
+                            if !playerName.isEmpty {
+                                saveScore()
+                            }
+                            resetGame()
+                        }
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        
+                        Button("Пропусни") {
+                            resetGame()
+                        }
+                        .font(.title)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    
+                } else {
+                    HStack(spacing: 5.0) {
+                        DiceView(number: diceOne, rolling: $rolling, held: holdDiceOne)
+                        DiceView(number: diceTwo, rolling: $rolling, held: holdDiceTwo)
+                        DiceView(number: diceThree, rolling: $rolling, held: holdDiceThree)
+                    }
+                    Spacer()
+                    if tokens > 0 {
+                        Button(action: {
+                            if resetAfterZeroPoints {
+                                resetAfterZeroPoints = false
+                            }
+                            rollDice()
+                        }) {
+                            Text(rolling ? "Въртене..." : "Хвърли заровете")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(rolling ? Color.blue : Color.green)
+                                .cornerRadius(10)
+                                .disabled(rolling)
+                        }
+                        Button("Запази точките") {
+                            savePoints()
+                        }
+                        .font(.largeTitle)
+                        .disabled(rolling || currentPoints == 0)
+                    }
                 }
                 Spacer()
-                if tokens > 0 {
-                    Button(action: {
-                        if resetAfterZeroPoints {
-                            resetAfterZeroPoints = false
+                
+                HStack {
+                    Button("Класация") {
+                        showLeaderboard = true
                         }
-                        rollDice()
-                    }) {
-                        Text(rolling ? "Въртене..." : "Хвърли заровете")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(rolling ? Color.blue : Color.green)
-                            .cornerRadius(10)
-                            .disabled(rolling)
+                        .font(.title)
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                                    
+                    Button("Нова игра") {
+                        resetGame()
+                        }
+                        .font(.title)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                     }
-                    Button("Запази точките") {
-                        savePoints()
-                    }
-                    .font(.largeTitle)
-                    .disabled(rolling || currentPoints == 0)
-                }
+                
             }
-            Spacer()
         }
-        .background(Color.pink)
+        .sheet(isPresented: $showLeaderboard) {
+            LeaderboardView(leaderboard: leaderboard)
+        }
+        .onAppear {
+            loadLeaderboard()
+        }
     }
     
     func rollDice() {
@@ -210,34 +267,88 @@ struct DiceGameView: View {
         holdDiceThree = false
     }
     
+    func saveScore() {
+        let newScore = PlayerScore(name: playerName, score: totalPoints)
+        leaderboard.append(newScore)
+        leaderboard.sort { $0.score > $1.score } // Подреждане на класацията по точки
+        saveLeaderboard()
+    }
+    
+    func saveLeaderboard() {
+        if let encoded = try? JSONEncoder().encode(leaderboard) {
+            UserDefaults.standard.set(encoded, forKey: "Leaderboard")
+        }
+    }
+    
+    func loadLeaderboard() {
+        if let savedData = UserDefaults.standard.data(forKey: "Leaderboard"),
+           let decoded = try? JSONDecoder().decode([PlayerScore].self, from: savedData) {
+            leaderboard = decoded
+        }
+    }
+    
     func resetGame() {
-        tokens = 10
-        totalPoints = 0
-        currentPoints = 0
-        holdDiceOne = false
-        holdDiceTwo = false
-        holdDiceThree = false
         diceOne = Int.random(in: 1...6)
         diceTwo = Int.random(in: 1...6)
         diceThree = Int.random(in: 1...6)
-        gameOver = false // Рестартираме флага за играта
+        currentPoints = 0
+        totalPoints = 0
+        lastRollPoints = 0
+        lostPoints = 0
+        holdDiceOne = false
+        holdDiceTwo = false
+        holdDiceThree = false
+        resetAfterZeroPoints = false
+        tokens = 10 // Възстановяване на токените
+        gameOver = false
+        playerName = "" // Изчистване на името на играча
     }
 }
 
 struct DiceView: View {
     let number: Int
     @Binding var rolling: Bool
-    let held: Bool
+    var held: Bool
     
     var body: some View {
         Image(systemName: "die.face.\(number).fill")
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 100, height: 100)
+            .frame(width: 80, height: 80)
             .rotationEffect(.degrees(rolling && !held ? 360 : 0))
             .animation(rolling && !held ? Animation.linear(duration: 0.15).repeatForever(autoreverses: false) : .default, value: rolling)
             .opacity(held ? 0.5 : 1.0)
             .padding()
+    }
+}
+struct PlayerScore: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let score: Int
+}
+
+struct LeaderboardView: View {
+    let leaderboard: [PlayerScore]
+    
+    var body: some View {
+        VStack {
+            Text("Класация")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white) // Променете цвета на текста, ако е необходимо
+            
+            List(leaderboard) { score in
+                HStack {
+                    Text(score.name)
+                    Spacer()
+                    Text("\(score.score) точки")
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .background(Color.clear) // Позволява цветът на родителския VStack да се вижда през списъка
+        }
+        .background(Color.blue) // Задайте желания цвят за фона на целия екран
+        .ignoresSafeArea() // Уверете се, че цветът на фона запълва целия екран
     }
 }
 
